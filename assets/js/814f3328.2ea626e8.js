9ccf68ddbd1efb3b56717bd727fb80579e8b0f05
huhn511 <huhn511@users.noreply.github.com>
2021-11-13 21:40:58 +0000

    deploy: 4ad686a764eecef3b476496cf62cedadd221c22c

---
+++ assets/js/814f3328.2ea626e8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdckirrberg=self.webpackChunkdckirrberg||[]).push([[535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Willkommen!","permalink":"/blog/welcome"}]}')}}]);
